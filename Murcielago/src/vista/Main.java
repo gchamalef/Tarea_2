09d6c8c8c7ee1bf05b847e9058c5ffe4c6c20c4a
Guillermo Chamale <gchamalef@miumg.edu.gt>
2023-08-22 02:18:00 -0600

    Inicio del proyecto, creacion y configuracion de la ventana, creacion de funciones y configuracion de eventos.

---
+++ Murcielago/src/vista/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author pc01
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_ventana frm = new frm_ventana();
+        frm.show();
+    }
+    
+}
